96af3e7f533b7e79db267da9337c5653a1411d0b
cmwmom <cmwmom@naver.com>
2021-01-22 22:15:07 +0900

    basic

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	vector<string> V{10, "hi"};
+	
+	for(auto c : V)
+		cout << c << '\n';
+	return 0;
+}
