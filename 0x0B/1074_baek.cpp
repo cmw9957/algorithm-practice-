dfcbd81200b2bf399f6bcab4a8ebaeb9c5002582
cmwmom <cmwmom@naver.com>
2021-02-03 19:58:43 +0900

    basic

--- 0x0B/1074_baek.cpp
+++ 0x0B/1074_baek.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int func(int n, int r, int c){
 	if(n == 0) return 0;
-	int half = 1<<(n-1);
+	int half = (1<<n-1);											//사각형을 반씩 잘라 구역을 나누기위해
 	if(r < half && c < half) return func(n-1, r, c);
-	if(r < half && c >= half) return half*half + func(n-1, r, c-half);
-	if(r >= half && c < half) return 2*half*half + func(n-1, r-half, c);
-	return 3*half*half + func(n-1, r-half, c-half);
-}
+	if(r >= half && c < half) return 2*half*half + func(n-1, r, c);
+	if(r < half && c >= half) return half*half + func(n-1, r, c);
+	return 3*half*half + func(n-1, r, c);
+} 
 
 int main(){
 	int n, r, c;
