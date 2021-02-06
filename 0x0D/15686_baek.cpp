71a3fff3c798cce2004ad5e6e71909f89fdc6964
cmwmom <cmwmom@naver.com>
2021-02-06 22:16:26 +0900

    basic

--- 0x0D/15686_baek.cpp
+++ 0x0D/15686_baek.cpp
@@ -18,11 +18,14 @@ int main(void){
 			else if(box[i][j] == 2) chicken.push_back({i, j});
 		}
 	}
+	//총 ~개중에서 m개만을 사용한다할때 백트래킹 사용, 경우의 수, 순열의 조합 등 
 	vector<int> brute(chicken.size(), 1);
 	fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+	//모든 경우의 수를 계산하여 도출한 최소값 
 	int total = 0x7f7f7f7f;
 	do{
 		int dist = 0;
+		//한 경우의 수에서 각 집마다 치킨거리를 구함 
 		for(auto h : house){
 			int temp = 0x7f7f7f7f;
 			for(int i = 0;i < chicken.size();i++){
@@ -31,6 +34,7 @@ int main(void){
 			}
 			dist += temp;
 		}
+		//많은 경우의 수 중에서 하나의 도시 치킨거리 최소값을 비교 
 		total = min(total, dist);
 	}while(next_permutation(brute.begin(), brute.end()));
 	cout << total;
--- 0x0D/brute_force_practice.cpp
+++ 0x0D/brute_force_practice.cpp
@@ -1,40 +1,87 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define R 12
+#define C 6
 #define X first
 #define Y second
-int n, m;
-int box[55][55];
-vector<pair<int, int> > chicken;
-vector<pair<int, int> > house;
+int explo[R][C];
+string box[R];
+bool vis[R][C];
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+void fall(){
+	for(int i = 0;i < C;i++){
+		char temp[R] = {};
+		int idx = 0;
+		fill(temp, temp + R, '.');
+		for(int j = R-1;j >= 0;j--)
+			if(box[j][i] != '.') 
+				temp[idx++] = box[j][i];
+		
+		idx = 0;
+		for(int j = R-1;j >= 0;j--) 
+			box[j][i] = temp[idx++];
+	}
+}
+
+void func(int num){
+	for(int i = 0;i < R;i++){
+		for(int j = 0;j < C;j++){
+			if(explo[i][j] == num) box[i][j] = '.';
+		}
+	}
+}
 
 int main(void){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> n >> m;
-	for(int i = 0;i < n;i++){
-		for(int j = 0;j < n;j++){
-			cin >> box[i][j];
-			if(box[i][j] == 1) house.push_back({i, j});
-			else if(box[i][j] == 2) chicken.push_back({i, j});
-		}
-	}
-	vector<int> brute(chicken.size(), 1);
-	fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
-	int total = 0x7f7f7f7f;
+	for(int i = 0;i < R;i++)
+		cin >> box[i];
+	int ans_cnt = 0;
+	bool boom = false;
 	do{
-		int dist = 0;
-		for(auto h : house){//m개의 치킨집이 남은 하나의 경우의 수에서 모든 집까지의 거리를 계산 
-			int temp = 0x7f7f7f7f;
-			for(int i = 0;i < chicken.size();i++){
-				if(brute[i] == 0) continue;
-				//m개의 치킨집중에서 가장 가까운 치킨집까지의 거리를 선별 
-				temp = min(temp, abs(h.X - chicken[i].X) + abs(h.Y - chicken[i].Y));
+		int num = 0;
+		boom = false;
+		for(int i = 0;i < R;i++){
+			for(int j = 0;j < C;j++){
+				
+				if(box[i][j] != '.' && !vis[i][j]){
+					num++;
+					explo[i][j] = num;
+					int boom_cnt = 0;
+					boom_cnt++;
+					vis[i][j] = 1;
+					queue<pair<int, int> > Q;
+					Q.push({i, j});
+					while(!Q.empty()){
+						auto cur = Q.front(); Q.pop();
+						for(int dir = 0;dir < 4;dir++){
+							int nx = cur.X + dx[dir];
+							int ny = cur.Y + dy[dir];
+							if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+							if(vis[nx][ny] || box[nx][ny] != box[cur.X][cur.Y]) continue;
+							explo[nx][ny] = num;
+							boom_cnt++;
+							vis[nx][ny] = 1;
+							Q.push({nx, ny});
+						}
+					}
+					if(boom_cnt >= 4){
+						boom = 1;
+						func(num);
+					}
+				}
+				
 			}
-			dist += temp;
 		}
-		//m개의 치킨집이 있는 경우의 수들 중 최소값을 선별 
-		total = min(total, dist);
-	}while(next_permutation(brute.begin(), brute.end()));
-	cout << total;
+		if(boom) ans_cnt++;
+		for(int i = 0;i < R;i++){
+			fill(vis[i], vis[i] + C, 0);
+			fill(explo[i], explo[i] + C, 0);
+		}
+		fall();
+	}while(boom);
+	cout << ans_cnt;
 	return 0;
 }
